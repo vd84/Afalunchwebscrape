3c191dc421a6a70700a4e7b385f227a7d729a2b9
Douglas Hammarstam <dogge20@hotmail.com>
2020-09-28 12:25:16 +0200

    gjort första anropet till sida

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using AngleSharp;
+using AngleSharp.Html.Parser;
 
-namespace Afalunchwebscrape
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Afalunchwebscrape {
+    class Program {
+        static async System.Threading.Tasks.Task Main (string[] args) {
+            // Load default configuration
+            var config = Configuration.Default.WithDefaultLoader ();
+            // Create a new browsing context
+            var context = BrowsingContext.New (config);
+            // This is where the HTTP request happens, returns <IDocument> that // we can query later
+            var document = await context.OpenAsync ("https://diwinestockholm.se/lunch/");
+            // Log the data to the console
+            System.Console.WriteLine(document.ChildNodes.ToHtml());
+            var siteHtml = document.ChildNodes.ToHtml();
+            //var dagensLunchRows = document.QuerySelectorAll();
         }
     }
 }
