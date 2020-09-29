3721f31a88824f0a1bcc536b40d8bdc1e8efc43b
Douglas Hammarstam <dogge20@hotmail.com>
2020-09-29 09:32:34 +0200

    cleans up code

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using AngleSharp;
-using AngleSharp.Html.Parser;
-using maträtter;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Threading.Tasks;
 using Diwine.Scrapers;
 
 namespace Afalunchwebscrape {
@@ -15,6 +8,8 @@ namespace Afalunchwebscrape {
             DiwineScrape diwine = new DiwineScrape();
             System.Console.WriteLine("Scraping diwine");
             var diwineLuncherDennaVecka = await diwine.Scrape();
+
+
         }
     }
 
